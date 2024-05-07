af8eaa00de28c68828fa25a3e481b593c0283685
Laurent VAYLET <laurent.vaylet@gmail.com>
2024-05-07 21:07:11 +0000

    bind application to random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    run(TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port."))?.await
 }
