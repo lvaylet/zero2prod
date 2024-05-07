5b442c92cfd32a11ce9b5930460c8de23f74f4d9
Laurent VAYLET <laurent.vaylet@gmail.com>
2024-05-07 20:09:07 +0000

    fix route template

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/name", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
