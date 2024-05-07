eeab0e45ffffd437e559890719c046fb3d0182d4
Laurent VAYLET <laurent.vaylet@gmail.com>
2024-05-07 20:30:25 +0000

    restructure for integration tests

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
