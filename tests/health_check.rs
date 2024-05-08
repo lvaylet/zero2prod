f729b56f4f6693463f176ad63d625ff28c39e801
Laurent VAYLET <laurent.vaylet@gmail.com>
2024-05-08 07:40:45 +0000

    document and move function

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,15 @@
 use std::net::TcpListener;
 
+/// Spin up an instance of our application
+/// and return its address (i.e. http://localhost:XXXX)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -13,12 +23,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
 }
