ddbb36bae43cf17f2bd09e1ae05bde082e68ebf7
Laurent VAYLET <laurent.vaylet@gmail.com>
2024-05-08 07:42:44 +0000

    add comments to integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,15 +12,18 @@ fn spawn_app() -> String {
 
 #[tokio::test]
 async fn health_check_works() {
+    // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
         .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
